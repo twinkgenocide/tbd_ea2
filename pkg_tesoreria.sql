ab709697d6f0119639e0301e787f6f91c3d00be9
twinkgenocide <illusivelove@proton.me>
2025-10-28 21:21:27 -0300

    Agregado Package

---
+++ pkg_tesoreria.sql
@@ -0,0 +1,98 @@
+-- Encabezado del paquete
+CREATE OR REPLACE PACKAGE pkg_tesoreria IS
+    PROCEDURE pr_registrar_error(
+        p_rutina VARCHAR2,
+        p_msjError VARCHAR2
+    );
+    FUNCTION fn_valorizacion_propiedad(
+        p_nroRol PROPIEDAD.NROROL%TYPE
+    ) RETURN NUMBER;
+    FUNCTION fn_incrementar_valorizado_propiedad(
+        p_nroRol PROPIEDAD.NROROL%TYPE,
+        p_porcentaje NUMBER
+    ) RETURN NUMBER;
+    FUNCTION fn_calcular_contribucion_propiedad (
+        p_nrorol IN PROPIEDAD.NROROL%TYPE
+    ) RETURN NUMBER;
+END pkg_tesoreria;
+/
+-- Cuerpo del paquete
+CREATE OR REPLACE PACKAGE BODY pkg_tesoreria IS
+    PROCEDURE pr_registrar_error(
+        p_rutina VARCHAR2,
+        p_msjError VARCHAR2
+    ) IS
+        v_correlativo NUMBER;
+    BEGIN
+        SELECT seq_error.NEXTVAL INTO v_correlativo FROM dual;
+        
+        INSERT INTO error_calc_avaluos (
+            correlativo,
+            rutina_error,
+            mensaje_error
+        ) VALUES (
+            v_correlativo,
+            p_rutina,
+            p_msjError
+        );
+    END;
+    
+    FUNCTION fn_valorizacion_propiedad(
+        p_nroRol PROPIEDAD.NROROL%TYPE
+    ) RETURN NUMBER IS
+        v_valorUF VALOR_UF.VALOR_EN_PESOS%TYPE;
+        v_superficie NUMBER;
+        v_valorizado NUMBER;
+    BEGIN
+        SELECT superficie INTO v_superficie
+        FROM Propiedad WHERE nrorol = p_nroRol;
+        
+        SELECT valor_en_pesos INTO v_valorUF
+        FROM Valor_UF WHERE fecha = (SELECT MAX(fecha) FROM valor_uf);
+        
+        v_valorizado := v_superficie * v_valorUF;
+        RETURN v_valorizado;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            pr_registrar_error(
+                'fn_valorizacion_propiedad',
+                'Propiedad con nroRol = ' || p_nroRol || ' no encontrada.');
+            RAISE;
+    END;
+    
+    FUNCTION fn_incrementar_valorizado_propiedad(
+        p_nroRol PROPIEDAD.NROROL%TYPE,
+        p_porcentaje NUMBER
+    ) RETURN NUMBER IS
+        v_valorizado NUMBER;
+        v_valorizado_incrementado NUMBER;
+    BEGIN
+        v_valorizado := fn_valorizacion_propiedad(p_nroRol);
+        v_valorizado_incrementado := v_valorizado * (1 + p_porcentaje / 100);
+        RETURN v_valorizado_incrementado;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            pr_registrar_error(
+                'fn_incrementar_valorizado_propiedad',
+                'Propiedad con nroRol = ' || p_nroRol || 'no encontrada.');
+            RAISE;
+    END;
+    
+    FUNCTION fn_calcular_contribucion_propiedad (
+        p_nrorol IN PROPIEDAD.NROROL%TYPE
+    ) RETURN NUMBER IS
+        v_valorizado NUMBER;
+        v_contribucion NUMBER;
+    BEGIN
+        v_valorizado := obtener_valorizado_propiedad(p_nrorol);
+        v_contribucion := v_valorizado * 0.16;
+        RETURN v_contribucion;
+    
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            pr_registrar_error(
+                'fn_calcular_contribucion_propiedad',
+                'Propiedad con nroRol = ' || p_nroRol || 'no encontrada.');
+            RAISE;
+    END;
+END pkg_tesoreria;
